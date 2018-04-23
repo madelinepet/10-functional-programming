c624eea93991a7fe5b75d339e048bdbb986d647e
MADELINE PETERS <MADELINEPET@HOTMAIL.COM>
2018-04-23 11:03:40 -0700

    We have articles displaying on the home page!

--- madeline-yannick/public/scripts/article.js
+++ madeline-yannick/public/scripts/article.js
@@ -6,28 +6,28 @@ var app = app || {};
     // REVIEW: In Lab 8, we explored a lot of new functionality going on here. Let's re-examine the concept of context. Normally, "this" inside of a constructor function refers to the newly instantiated object. However, in the function we're passing to forEach, "this" would normally refer to "undefined" in strict mode. As a result, we had to pass a second argument to forEach to make sure our "this" was still referring to our instantiated object. One of the primary purposes of lexical arrow functions, besides cleaning up syntax to use fewer lines of code, is to also preserve context. That means that when you declare a function using lexical arrows, "this" inside the function will still be the same "this" as it was outside the function. As a result, we no longer have to pass in the optional "this" argument to forEach!
     Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
   }
-  
+
   Article.all = [];
-  
+
   Article.prototype.toHtml = function() {
     var template = Handlebars.compile($('#article-template').text());
-    
+
     this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
     this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
     this.body = marked(this.body);
-    
+
     return template(this);
   };
-  
+
   Article.loadAll = articleData => {
     articleData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
-    
+
     /* OLD forEach():
     articleData.forEach(articleObject => Article.all.push(new Article(articleObject)));
     */
-    let newArticle = articleData.map()
+    Article.all = articleData.map(articleObject => new Article(articleObject));
   };
-  
+
   Article.fetchAll = callback => {
     $.get('/articles')
       .then(results => {
@@ -35,11 +35,11 @@ var app = app || {};
       })
       .then(callback);
   };
-  
+
   Article.numWordsAll = () => {
     return Article.all.map().reduce()
   };
-  
+
   Article.allAuthors = () => {
     return Article.all.map().reduce();
   };
